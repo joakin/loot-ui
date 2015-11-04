35ea307f08f3139aa73764bf925de21d5180e997
joakin <joaquin@chimeces.com>
2015-11-04 12:57:24 +0100

    Set babel compilation on the server
    In anticipation to loading es2015 client side code soon.

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-var http = require('http')
-var url = require('url')
-var ecstatic = require('ecstatic')({
-  root: './public',
-  showDir: false
-})
+require('babel-core/register')
 
-var router = require('./lib/routes')
-var port = process.env.PORT || 7002
+global.__CLIENT__ = false
+global.__SERVER__ = true
 
-http.createServer((req, res) => {
-  // Route request to corresponding endpoint
-  var path = url.parse(req.url).pathname
-  var match = router.match(path)
-  if (match) match.fn(req, res, match)
-  else ecstatic(req, res)
-}).listen(port, () => console.log('Listening at', port))
+require('./lib/server/index')
