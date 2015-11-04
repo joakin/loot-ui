cf455d00dd8be9b1c8c02da44443d941ce33e51c
joakin <joaquin@chimeces.com>
2015-11-04 17:23:08 +0100

    Only compile assets in the server in dev mode
    Do not use webpack otherwise. In preproductions assets will need to be
    precompiled.

--- webpack/base.webpack.config.js
+++ webpack/base.webpack.config.js
@@ -3,19 +3,20 @@ var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
     './lib/client/index'
   ],
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.resolve('./public'),
     filename: 'bundle.js',
     publicPath: '/'
   },
   plugins: [
+    new webpack.DefinePlugin({__CLIENT__: true, __SERVER__: false}),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: process.env.NODE_ENV}
+    }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new ExtractTextPlugin('style.css', { allChunks: true })
   ],
@@ -23,12 +24,15 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loaders: [ 'babel' ],
+        loaders: ['babel'],
         exclude: /node_modules/,
-        include: __dirname
+        include: path.resolve('./')
       },
       { test: /\.(gif|png|jpg)$/, loader: 'url?limit=25000' },
-      { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!less') }
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!less')
+      }
     ]
   }
 }
---
+++ webpack/dev.webpack.config.js
@@ -0,0 +1,9 @@
+var webpack = require('webpack')
+var conf = module.exports = require('./base.webpack.config')
+
+conf.devtool = 'source-map'
+conf.entry = ['webpack-hot-middleware/client'].concat(conf.entry)
+conf.plugins = conf.plugins.concat(
+  new webpack.HotModuleReplacementPlugin()
+)
+
