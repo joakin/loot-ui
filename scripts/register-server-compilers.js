6ab2448868259c28d477a3095495640517c2a595
joakin <joaquin@chimeces.com>
2015-12-09 12:59:37 +0100

    Add /about route to show the app version
    To see in deployed cached versions the version and see if it updates with
    service worker.

--- scripts/register-server-compilers.js
+++ scripts/register-server-compilers.js
@@ -1,5 +1,9 @@
 require('babel-core/register')
 require.extensions['.less'] = function () { return null }
+var fs = require('fs')
 
+var packageConfig = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
+
+global.__VERSION__ = packageConfig.version
 global.__CLIENT__ = false
 global.__SERVER__ = true
