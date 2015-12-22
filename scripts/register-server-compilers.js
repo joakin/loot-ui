242a6c2a96fb7f98784f98eddb14c17b02e4aa0e
joakin <joaquin@chimeces.com>
2015-12-22 13:03:43 +0100

    Test that <Article/> works with a null doc
    Which is passed in when reloading a service worker enabled page.

--- scripts/register-server-compilers.js
+++ scripts/register-server-compilers.js
@@ -7,3 +7,9 @@ var packageConfig = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
 global.__VERSION__ = packageConfig.version
 global.__CLIENT__ = false
 global.__SERVER__ = true
+
+var jsdom = require('jsdom')
+
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.window = document.defaultView
+global.navigator = {userAgent: 'node.js'}
